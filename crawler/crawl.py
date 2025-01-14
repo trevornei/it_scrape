bc5570c3ac5788ed6e139f27708bd65b9a1f569f
trevornei <117675657+trevornei@users.noreply.github.com>
2025-01-14 15:38:28 -0700

    Major Changes... Added color styling to print statements to help with readability in the terminal. Added the crawl_child_elements fn to crawl for child urls and place them in a nested dictionary inside the dont_replicated list moved the save file statement to the bottom. Added the date & time in which the file was saved to help debug and give context to anyone who visits the file.

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -50,13 +50,50 @@ dont_replicate = []
 # Removes repetitive items in the list of urls.
 def anti_replicator ():
     for url in https_urls:
-        if url not in dont_replicate:
-            dont_replicate.append(url)
+        if not any(d['Parent URL: '] == url for d in dont_replicate):
+            dont_replicate.append({
+                "Parent URL: ": url
+            })
             # print(dont_replicate)
         else:
-            print(f"This url was a dublicate ---x {url}")
+            print(f"\033[31mThis url was a duplicate ---x \033[0m {url}")
             continue
 
-
 anti_replicator()
-print(len(dont_replicate))
+print(f"\n\033[92mThis is new the culled list:\033[0m \033[94m{len(dont_replicate)}\033[0m {dont_replicate}")
+
+
+
+
+""" Now Iterate over the cleaned list of URL's. """
+
+def crawl_child_elements(depth=0, max_depth=20):
+    # iterate through the elements in cleaned list. 
+    for p in dont_replicate:
+        sidebar_elements = soup.find_all('div', class_='sidebar-template')
+        for element in sidebar_elements:
+            c_href = element.find_all('a', href=True)
+            for link in c_href:
+                child_href_value = link.get('href', '')
+                if any(d['Parent URL: '] == child_href_value for d in dont_replicate):
+                    print(f"\033[31mThis child url is a duplicate ---x \033[0m {child_href_value}")
+                else:
+                    print(f"\033[92mThis child url is unique:\033[0m {child_href_value}")
+                    child_elements = {f"Child URL {len(dont_replicate) + 1}": child_href_value}
+                    dont_replicate.append({
+                        "Parent URL: ": child_href_value,
+                        "Child Elements": child_elements
+                    })
+
+crawl_child_elements()
+
+def save_to_file(filename):
+    with open(filename, 'w') as file:
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            file.write(f"# Saved on: {timestamp}\n")
+            file.write(f"dont_replictate = {dont_replicate}")
+
+# Example usage
+save_to_file('cleaned_urls.txt')
+
+print(dont_replicate)
